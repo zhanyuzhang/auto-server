0d91f6b889da006d7a767326cf539b23a01f19ee
chesszhang <1130142064@qq.com>
2018-11-15 11:13:26 +0800

    fixed: exclude node_modules and show the changed files

--- bin/gulpfile.js
+++ bin/gulpfile.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const gulp = require('gulp');
 const connect = require('gulp-connect');
 const open = require('open');
+const chalk = require('chalk');
 
 const argv = process.argv;
 const cwd = argv[argv.indexOf('--cwd') + 1];
@@ -17,22 +18,28 @@ gulp.task('connect', function() {
       port: 3000 + parseInt(Math.random() * 1000),
     },
   }, function() {
+    console.log(chalk.yellow('Available on:\n'), `http://${host}:` + chalk.green(port));
     open(`http:${host}:${port}`);
   });
 });
 
+// Globs to watch on the file system.
 const suffixList = ['.html', '.css', '.js', '.png', '.jpg', '.gif'];
-const globs = suffixList.map((suffix) => `${cwd.replace(/\\/g, '/')}/**/*${suffix}`);
+const globs =  suffixList.map((suffix) => `${cwd.replace(/\\/g, '/')}/**/*${suffix}`);
+// Exclude the node_modules
+globs.push(`!${cwd.replace(/\\/g, '/')}/**/node_modules/*`);
 
-// Cache files content，when press `ctrl+s` will trigger gulp.change event, although file has no change,
-// so we should compare the file content to judge whether it has changed to reload the page.
+// Cache files content，when press `ctrl+s` will trigger gulp.change event although file has no chane,
+// so we should compare the file content to judge whether it has changed.
 const fileContentCache = {};
+
 gulp.task('watch', function () {
   gulp.watch(globs, function(event) { 
     const filePath = event.path;
       try {
         const fileContent = fs.readFileSync(event.path, 'utf8');
         if(fileContent !== fileContentCache[filePath]) {
+          console.log(filePath, 'has', chalk.yellow(event.type));
           fileContentCache[filePath] = fileContent;
           gulp.src(globs)
           .pipe(connect.reload())
